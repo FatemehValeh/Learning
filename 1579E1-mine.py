72b4bb44f9169f659e7a7157be44d765e374cd76
FatemehValeh <fatemehvaleh@gmail.com>
2021-11-01 10:24:05 +0330

    Add cleaner code for 1579E1-mine

--- 1579E1-mine.py
+++ 1579E1-mine.py
@@ -1,14 +1,14 @@
 from collections import deque
 t = int(input())
 for i in range(t):
-    n = int(input())
-    arr = [int(el) for el in input().split()]
+    _ = int(input())
+    array = [int(el) for el in input().split()]
     answer = deque([])
-    answer.append(arr[0])
-    for j in range (1, n):
-        if arr[j] > answer[0]:
-            answer.append(arr[j])
+    answer.append(array[0])
+    for item in array[1:]:
+        if item > answer[0]:
+            answer.append(item)
         else:
-            answer.appendleft(arr[j])
+            answer.appendleft(item)
             
     print(*answer, sep=' ')
